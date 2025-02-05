f13f0f674e337f2e10a97306a97d3460f91b8794
oltionnamoni <oltionnamoni73@gmail.com>
2025-02-05 03:14:48 -0800

    s

---
+++ Shapes Challenges/main.js
@@ -0,0 +1,14 @@
+var btn_circle=document.getElementByld("btnCircle");
+var btn_rect=document.getElementByld("btnrect");
+var btn_triangle=document.getElementByld("btnTriangle");
+
+var circle=document.getElementByld("triangle");
+
+btn_circle.onclick=funcction()
+{
+    circle.setAttribute("class", "shape_circle");
+}
+btn_react.onclick=function()
+{
+    circle.setAttribute("class", "shape_rect");
+}
