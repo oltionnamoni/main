9758f6c55511db5ff49f556cf3949a79844e47a2
oltionnamoni <oltionnamoni73@gmail.com>
2025-02-10 03:02:44 -0800

    a

---
+++ LESSON29/main.js
@@ -0,0 +1,45 @@
+var programmingLanguages = ['Java script','php','python'];
+
+console.log(programmingLanguages);
+
+console.log(programmingLanguages[0]);
+programmingLanguages.push('Java');
+console.log(programmingLanguages);
+
+programmingLanguages.pop();
+console.log(programmingLanguages);
+
+
+programmingLanguages.unshift('c#');
+console.log(programmingLanguages);
+
+programmingLanguages.shift();
+console.log(programmingLanguages);
+
+programmingLanguages.splice(0,2, 'Ruby');
+console.log(programmingLanguages);
+
+console.log(Math.random()*5);
+console.log(Math.floor(Math.random()*5));
+
+var students=['Anid','Reald']
+
+var[s1,s2]=students;
+console.log(s1);
+console.log(s2);
+
+
+var places=['London','Paris','New York','Berlin'];
+
+var[firstPlace,secondPlace]=places;
+
+console.log(secondPlace);
+
+
+var numbers=[1,2,3,4,5,6,7,8,9,10];
+
+var[first, second, ...otherNumbers]=numbers;
+
+console.log(first);
+console.log(second);
+console.log(otherNumbers.toString());
